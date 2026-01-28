e1ef551f24b9a5ba1eaaf5b3795f8186aef84970
arnolder <arnoldermorozov@yandex.ru>
2026-01-28 21:33:47 +0300

    init: скопировал все компоненты из предыдущего проекта

---
+++ src/components/SidebarNav.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="sidebar" :style="themeStyles">
+    <ul class="menu">
+      <li><a href="#" :style="themeStyles">Главная</a></li>
+      <li><a href="#" :style="themeStyles">Летательные аппараты</a></li>
+      <li><a href="#" :style="themeStyles">Парашютные системы</a></li>
+      <li><a href="#" :style="themeStyles">Школа пилотирования</a></li>    
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, type CSSProperties, type PropType } from 'vue'
+
+export default defineComponent({
+  name: 'SidebarNav',
+  props: {
+    themeStyles: {
+      type: Object as PropType<CSSProperties>,
+      default: () => ({})
+    }
+  }
+})
+</script>
+
+<style scoped>
+.sidebar {
+  padding: 10px;
+  color: inherit;
+}
+.menu {
+  list-style: none;
+}
+li {
+  margin: 5px;
+}
+a {
+  color: black;
+}
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,84 @@
+<template>
+  <MainLayout
+    @toggleTheme="toggleTheme"
+    :switchThemeHeaderFooter="switchThemeHeaderFooter"
+    :switchThemeMain="switchThemeMain"
+    :buttonsTheme="buttonsTheme"
+  >
+    <MainContent
+        class="border"
+        title="Летательные аппараты"
+        :theme-styles="switchTheme"
+        :buttonsTheme="buttonsTheme"
+      />
+      <SidebarNav
+        :theme-styles="switchTheme"
+        class="border"
+      />
+  </MainLayout>
+</template>
+
+<script lang="ts">
+import MainContent from '@/components/MainContent.vue'
+import MainLayout from '@/components/layouts/MainLayout.vue'
+import SidebarNav from '@/components/SidebarNav.vue'
+import { defineComponent, type CSSProperties } from 'vue'
+
+export default defineComponent({
+  name: 'MainView',
+  components: { SidebarNav, MainContent, MainLayout},
+  data() {
+    return {
+      isDarkTheme: false,
+    }
+  },
+  computed: {
+    switchTheme(): CSSProperties {
+      return {
+          'background-color': this.isDarkTheme ? '#36373a' : 'white',
+          color: this.isDarkTheme ? 'darkgray' : 'black',
+        }
+    },
+    switchThemeMain(): CSSProperties {
+      return {
+          'background-color': this.isDarkTheme ? 'darkgray' : undefined,
+        }
+    },
+    switchThemeHeaderFooter(): CSSProperties {
+      return {
+          'background-color': this.isDarkTheme ? '#36373a' : 'rgba(13, 140, 190, 0.5)',
+          color: this.isDarkTheme ? 'darkgray' : 'black',
+        }
+    },
+    buttonsTheme(): CSSProperties {
+      return {
+          'background-color': this.isDarkTheme ? 'gray' : undefined,
+        }
+    }
+  },
+  methods: {
+    toggleTheme(event: boolean) {
+      this.isDarkTheme = event
+      console.log(event)
+    },
+  },
+})
+</script>
+
+<style scoped>
+.root {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.container {
+  display: flex;
+  column-gap: 10px;
+  flex-grow: 1;
+  padding: 10px;
+}
+.border {
+  border: 3px solid gray;
+}
+</style>
