b75ec02263c6b86ed52cdcd02a9c4a2172a3a651
arnolder <arnoldermorozov@yandex.ru>
2026-01-29 21:30:24 +0300

    feat: MainView to Composition API

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -6,63 +6,50 @@
     :buttonsTheme="buttonsTheme"
   >
     <MainContent
-        class="border"
-        title="Летательные аппараты"
-        :theme-styles="switchTheme"
-        :buttonsTheme="buttonsTheme"
-      />
-      <SidebarNav
-        :theme-styles="switchTheme"
-        class="border"
-      />
+      class="border"
+      title="Летательные аппараты"
+      :theme-styles="switchTheme"
+      :buttonsTheme="buttonsTheme"
+    />
+    <SidebarNav :theme-styles="switchTheme" class="border" />
   </MainLayout>
 </template>
 
-<script lang="ts">
-import MainContent from '@/components/composition/MainContent.vue'
-import MainLayout from '@/components/composition/layouts/MainLayout.vue'
-import SidebarNav from '@/components/composition/SidebarNav.vue'
-import { defineComponent, type CSSProperties } from 'vue'
+<script setup lang="ts">
+import MainContent from '@/components/composition/MainContent.vue';
+import MainLayout from '@/components/composition/layouts/MainLayout.vue';
+import SidebarNav from '@/components/composition/SidebarNav.vue';
+import { ref, computed, type CSSProperties } from 'vue';
 
-export default defineComponent({
-  name: 'MainView',
-  components: { SidebarNav, MainContent, MainLayout},
-  data() {
-    return {
-      isDarkTheme: false,
-    }
-  },
-  computed: {
-    switchTheme(): CSSProperties {
-      return {
-          'background-color': this.isDarkTheme ? '#36373a' : 'white',
-          color: this.isDarkTheme ? 'darkgray' : 'black',
-        }
-    },
-    switchThemeMain(): CSSProperties {
-      return {
-          'background-color': this.isDarkTheme ? 'darkgray' : undefined,
-        }
-    },
-    switchThemeHeaderFooter(): CSSProperties {
-      return {
-          'background-color': this.isDarkTheme ? '#36373a' : 'rgba(13, 140, 190, 0.5)',
-          color: this.isDarkTheme ? 'darkgray' : 'black',
-        }
-    },
-    buttonsTheme(): CSSProperties {
-      return {
-          'background-color': this.isDarkTheme ? 'gray' : undefined,
-        }
-    }
-  },
-  methods: {
-    toggleTheme(event: boolean) {
-      this.isDarkTheme = event
-      console.log(event)
-    },
-  },
-})
+const isDarkTheme = ref(false);
+
+// вычисляемые свойства
+const switchTheme = computed((): CSSProperties => {
+  return {
+    'background-color': isDarkTheme.value ? '#36373a' : 'white',
+    color: isDarkTheme.value ? 'darkgray' : 'black',
+  };
+});
+const switchThemeMain = computed((): CSSProperties => {
+  return {
+    'background-color': isDarkTheme.value ? 'darkgray' : undefined,
+  };
+});
+const switchThemeHeaderFooter = computed((): CSSProperties => {
+  return {
+    'background-color': isDarkTheme.value ? '#36373a' : 'rgba(13, 140, 190, 0.5)',
+    color: isDarkTheme.value ? 'darkgray' : 'black',
+  };
+});
+const buttonsTheme = computed((): CSSProperties => {
+  return {
+    'background-color': isDarkTheme.value ? 'gray' : undefined,
+  };
+});
+
+// методы
+const toggleTheme = (event: boolean) => (isDarkTheme.value = event);
+
 </script>
 
 <style scoped>
