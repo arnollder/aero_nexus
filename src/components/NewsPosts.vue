9c84dfbaf5e5debbe6b81af71afbaebf5472d84c
arnolder <arnoldermorozov@yandex.ru>
2026-03-04 02:03:05 +0300

    style: NewsPosts стилизовал кнопки навигации

--- src/components/NewsPosts.vue
+++ src/components/NewsPosts.vue
@@ -1,9 +1,8 @@
 <template>
-  <div class="buttoms">
-    <button class="btn" @click="router.push({name: 'home'})">НА ГЛАВНУЮ</button>
+  <div class="buttons">
+    <button class="btn" @click="router.push({ name: 'home' })">НА ГЛАВНУЮ</button>
     <button class="btn" @click="router.back()">НАЗАД</button>
     <button class="btn" @click="router.forward()">ВПЕРЕД</button>
-
   </div>
   <div>Posts</div>
 </template>
@@ -16,7 +15,17 @@ const route = useRoute();
 </script>
 
 <style scoped>
+.buttons {
+  padding: 10px 20px;
+  display: flex;
+  column-gap: 10px;
+}
 .btn {
-  
+  padding: 5px 10px;
+  background-color: rgb(44, 172, 44);
+  color: black;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 }
 </style>
