3fdfbb3fcc474cb0fd145688661f795ed81558ab
arnolder <arnoldermorozov@yandex.ru>
2026-03-04 20:41:59 +0300

    fix: пофиксил лейаут

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,9 +8,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('@/views/MainView.vue'),
-      meta: {
-        layout: MainLayout,
-      },
     },
     {
       path: '/news',
