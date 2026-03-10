d12db5a0687c4486fb4d04eb4878788c6535c9ef
arnollder <arnoldermorozov@gmail.ru>
2026-03-10 15:49:58 +0300

    feat: WeatherStack добавил Минск и Москву

--- src/views/WeatherStack.vue
+++ src/views/WeatherStack.vue
@@ -1,10 +1,24 @@
 <template>
-  <!-- Нижний Новгород -->
-    <WeatherWidget
-      :apiKey="API_KEY"
-      lat="56.3264816"
-      lon="44.0051395"
-    />
+  <div class="widgets">
+    <!-- Нижний Новгород -->
+      <WeatherWidget
+        :apiKey="API_KEY"
+        lat="56.3264816"
+        lon="44.0051395"
+      />
+    <!-- Москва -->
+      <WeatherWidget
+        :apiKey="API_KEY"
+        lat="55.7522"
+        lon="37.6156"
+      />
+    <!-- Минск -->
+      <WeatherWidget
+        :apiKey="API_KEY"
+        lat="53.9"
+        lon="27.5667"
+      />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -15,5 +29,9 @@ const API_KEY = '424417532801ae277b2545ae0c4735c6'
 </script>
 
 <style scoped>
-
+.widgets {
+  margin: auto;
+  display: flex;
+  column-gap: 15px;
+}
 </style>
